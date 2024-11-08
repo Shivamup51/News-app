4f0f936da143bbaf2edd0b78044d0a2a3bdde883
shivam upadhyay <shivamupadhyay2868@gmail.com>
2024-11-09 04:52:41 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react"
-import Navbar from "./Components/Navbar"
-import NewsBoard from "./Components/NewsBoard"
+
+import { useState } from 'react';
+import Navbar from './Components/Navbar';
+import NewsBoard from './Components/NewsBoard';
+import './index.css';
 
 const App = () => {
-  const [category,setCategory]=useState("general");
+  const [category, setCategory] = useState("technology");
+
   return (
     <div>
-      <Navbar setCategory={setCategory}/>
-      <NewsBoard category={category}/>
+      <Navbar setCategory={setCategory} />
+      <NewsBoard category={category} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
